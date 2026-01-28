8714c80f982805ea5592a31f5a7886170c71bcc9
srele96 <srele96@gmail.com>
2026-01-28 01:31:54 +0100

    add sdl and testing code

--- README.md
+++ README.md
@@ -16,10 +16,32 @@ node
 
 ### Commands
 
+Clone the repo:
+
+```bash
+git clone --recursive your-repo
+```
+
+Otherwise, update submodules:
+
+```bash
+git submodule update --init --recursive
+```
+
+Install dependencies:
+
+Fedora:
+
+We have to install `SDL2-devel` dependency because it provides proper cmake configuration which allows cmake to find and link it.
+
+```bash
+sudo dnf install SDL2 SDL2-devel mesa-libGL mesa-libGL-devel glfw glfw-devel
+```
+
 First time:
 
 ```bash
-cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake -S . -B build
 ```
 
 When adding files:
--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
+#include <SDL_video.h>
 #include <iostream>
+#include "imgui.h"
+#include "backends/imgui_impl_sdl2.h"
+#include "SDL2/SDL.h"
 
 int main() {
-  std::cout << "Hello World" << "\n";
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
+    std::cout << "SDL_Init failed: " << SDL_GetError();
+    return 1;
+  }
+  IMGUI_CHECKVERSION();
+
+  SDL_Window* window = SDL_CreateWindow(
+    "Test",
+    SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED,
+    1280,
+    720,
+    SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
+  );
+
+  // TODO: Test this implementation
 
   return 0;
 }
